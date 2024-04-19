ff39a44f0ba673fc2d27d8b6d0e3fa41b73bebaf
trannhanchanh <chanhtnde160156@fpt.edu.vn>
2024-04-19 18:06:11 +0700

    message

--- casestudymodule3.sql
+++ casestudymodule3.sql
@@ -254,6 +254,33 @@ group by month(ngay_lam_hop_dong) ;
  group by hop_dong.ma_hop_dong;
  
  -- 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
- select dich_vu_di_kem.ten
+ select dich_vu_di_kem.ten_dich_vu_di_kem, dich_vu_di_kem.gia, dich_vu_di_kem.don_vi
+ from hop_dong 
+ inner join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong 
+ inner join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+ inner join khach_hang on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+ inner join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+ where loai_khach.ten_loai_khach = 'Diamond' and (khach_hang.dia_chi like '%Vinh%' or khach_hang.dia_chi like '%Quảng Ngãi%');
+ 
+ -- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+select hd.ma_hop_dong as id, nv.ho_ten, kh.ho_ten, kh.so_dien_thoai, dv.ma_dich_vu, dv.ten_dich_vu, ifnull(sum(hdct.so_luong),0) as quantity, hd.tien_dat_coc
+from hop_dong hd join khach_hang kh using (ma_khach_hang)
+join nhan_vien nv using (ma_nhan_vien)
+join dich_vu dv using(ma_dich_vu)
+left join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong_chi_tiet
+where hd.ma_hop_dong in(
+select ma_hop_dong
+from hop_dong 
+where year(ngay_lam_hop_dong)= 2020 and month(ngay_lam_hop_dong) between 10 and 12
+)
+and hd.ma_hop_dong not in(
+select ma_hop_dong
+from hop_dong
+where year(ngay_lam_hop_dong) = 2021 and month(ngay_lam_hop_dong) between 1 and 6
+)
+group by hd.ma_hop_dong;
+
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+
 
 
